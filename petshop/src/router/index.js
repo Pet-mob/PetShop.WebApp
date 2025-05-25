3f2b8dc0d64b91837420fa34a69ee43523ab322d
Rennan <rennan-castanheira@hotmail.com>
2025-05-25 11:41:51 -0300

    login

--- petshop/src/router/index.js
+++ petshop/src/router/index.js
@@ -1,25 +1,81 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/pages/LoginPage.vue'),
+    meta: { menu: false }
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import('@/pages/DashboardPage.vue'),
+    meta: { menu: true }
+  },
+  // {
+  //   path: '/agenda',
+  //   name: 'Agenda',
+  //   component: () => import('@/views/AgendaView.vue'),
+  //   meta: { menu: true }
+  // },
+  // {
+  //   path: '/fluxo-de-caixa',
+  //   name: 'FluxoDeCaixa',
+  //   component: () => import('@/views/FluxoDeCaixa.vue'),
+  //   meta: { menu: true }
+  // },
+  // {
+  //   path: '/',
+  //   redirect: '/dashboard'
+  // },
+  // {
+  //   path: '/:catchAll(.*)',
+  //   name: 'NotFound',
+  //   component: () => import('@/views/NotFound.vue'),
+  //   meta: { menu: false }
+  // }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = !!localStorage.getItem('token')
+  if (to.path !== '/login' && !isAuthenticated) {
+    next('/login')
+  } else {
+    next()
+  }
+})
+
 export default router
+
+
+// import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'home',
+//     component: HomeView
+//   },
+//   {
+//     path: '/about',
+//     name: 'about',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+//   }
+// ]
+
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes
+// })
+
+// export default router
