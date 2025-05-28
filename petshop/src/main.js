90c8471905893cec3f3640487628cce3f22a8edb
Rennan <rennan-castanheira@hotmail.com>
2025-05-27 21:10:48 -0300

    ajustes

--- petshop/src/main.js
+++ petshop/src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
 // import '../src/style/principal.css'
 // import '@/assets/css/principal.css'
 
 const app = createApp(App);
-
+app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
